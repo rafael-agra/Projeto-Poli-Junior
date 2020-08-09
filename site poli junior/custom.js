3bc0ea3ce7af0e723c72abb7cf64f21e1d86899a
rafaelmspu2 <rafaelmspu2@gmail.com>
2020-08-09 01:35:43 -0300

    Adicionada tabela

--- site poli junior/custom.js
+++ site poli junior/custom.js
@@ -15,11 +15,8 @@ getContent()
 
 function show(users){
     let output = ''
-    
+    output += `<table width="467" border="1px">`
     for (let user of users){
-        output += `<li>Nota da P1 -> ${user.nota_p1} </li> `;
-        output += `<li>Nota da P2 -> ${user.nota_p2} </li>`;
-        
         var n1 = parseFloat(`${user.nota_p1}`);
         var n2 = parseFloat(`${user.nota_p2}`);
         
@@ -31,12 +28,16 @@ function show(users){
 
         var n3 = ((mp*(p1+p2+p3)-p1*n1-p2*n2)/p3).toFixed(1);
         
+        output += `<tr> <td width="101" height="40"><li>Nota da P1 -> ${user.nota_p1} </li> </td>`;
+        output += `<td> <li>Nota da P2 -> ${user.nota_p2} </li> </td>`;
+
+        
         if (n3 <= 0){
-            output += `voce ja esta aprovado nesta disciplina `;
+            output += `<td>voce ja esta aprovado nesta disciplina</td> `;
         }else if(n3 >= 10){
-            output += `nao e possivel atingir a media proposta na p3`;
+            output += `<td>nao e possivel atingir a media proposta na p3</td>`;
         }else{
-            output += `a nota para a p3 eh `+ n3;
+            output += `<td>a nota para a p3 eh `+ n3 + '</td>';
         }
 
         
