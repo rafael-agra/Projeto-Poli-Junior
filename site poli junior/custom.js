504fd00e2ee167d61edc4e9ebec8f978def8e994
rafaelmspu2 <rafaelmspu2@gmail.com>
2020-08-09 00:58:03 -0300

    Calculo das notas

--- site poli junior/custom.js
+++ site poli junior/custom.js
@@ -20,11 +20,26 @@ function show(users){
         output += `<li>Nota da P1 -> ${user.nota_p1} </li> `;
         output += `<li>Nota da P2 -> ${user.nota_p2} </li>`;
         
-        var p2 = parseFloat(`${user.nota_p2}`);
-        var p1 = parseFloat(`${user.nota_p1}`);
-        var soma = p1+p2;
+        var n1 = parseFloat(`${user.nota_p1}`);
+        var n2 = parseFloat(`${user.nota_p2}`);
+        
+        var mp = parseFloat(`${user.media_pretendida}`);
+        
+        var p1 = parseFloat(`${user.peso_p1}`);
+        var p2 = parseFloat(`${user.peso_p2}`);        
+        var p3 = parseFloat(`${user.peso_p3}`);        
 
-        output += `a soma da p1 com a p2 eh `+ soma;
+        var n3 = ((mp*(p1+p2+p3)-p1*n1-p2*n2)/p3).toFixed(1);
+        
+        if (n3 <= 0){
+            output += `voce ja esta aprovado nesta disciplina `;
+        }else if(n3 >= 10){
+            output += `nao e possivel atingir a media proposta na p3`;
+        }else{
+            output += `a nota para a p3 eh `+ n3;
+        }
+
+        
     }
 
     document.querySelector('main').innerHTML = output;
