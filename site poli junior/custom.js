0217022955ee64fb200a120102e78c9addbebaf9
rafaelmspu2 <rafaelmspu2@gmail.com>
2020-08-11 00:24:23 -0300

    alteracoes gerais
    feita a centralizacao de objetos
    alterado o logo
    alteracoes na tabela
    e outras alteracoes menores

--- site poli junior/custom.js
+++ site poli junior/custom.js
@@ -1,3 +1,4 @@
+//Função assincrona
 async function getContent(){
     try {
         const response = await fetch ('https://api.polijunior.com.br/notas/')
@@ -15,11 +16,11 @@ getContent()
 
 function show(users){
     let output = ''
-    output += `<table width="467" border="1px">`;
-    output += `<tr> <td> Materia </td>`;
-    output += `<td> Nota na p1 </td>`;
-    output += `<td> Nota na p2 </td>`;
-    output += `<td> Nota necessaria na p3 </td>`;
+    output += `<table border="1px">`;
+    output += `<tr> <td> <h3>Matéria</h3> </td>`;
+    output += `<td> <h3>Nota na P1 </h3></td>`;
+    output += `<td> <h3>Nota na P2 </h3></td>`;
+    output += `<td> <h3>Nota necessária na P3</h3> </td>`;
     for (let user of users){
         var n1 = parseFloat(`${user.nota_p1}`);
         var n2 = parseFloat(`${user.nota_p2}`);
@@ -40,7 +41,7 @@ function show(users){
         if (n3 <= 0){
             output += `<td width = "400">Você já está aprovado nesta disciplina :)</td> `;
         }else if(n3 >= 10){
-            output += `<td>Não é possível atingir a média proposta na p3</td>`;
+            output += `<td>Não é possível atingir a média proposta na p3 :(</td>`;
         }else{
             output += `<td>A nota necessária para a p3 é de: `+ n3 + '</td>';
         }
